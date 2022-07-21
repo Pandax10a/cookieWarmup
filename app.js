7806e7acde0b2a2b3999d2b0f95f4547f1bd5c6d
panda1 <blueblue0011@gmail.com>
2022-07-21 15:28:38 -0700

    at 6.3.2 stuck not able to add +1

---
+++ app.js
@@ -0,0 +1,12 @@
+
+Cookies.set(`click_count`, 0);
+let click_me_event = document.querySelector(`#click_1`);
+click_me_event.addEventListener(`click`, click_me_event_count);
+function click_me_event_count() {
+    Cookies.get(`click_count`);
+    Cookies.set(`click_count`, +1);
+
+    // let true_count = Cookies.get(`click_count`);
+    // console.log(true_count);
+    
+}
